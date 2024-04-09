6518134083138ede9e0a8943818de987b841fbcd
MGamal <mg68163@gmail.com>
2024-04-09 17:39:29 +0200

    updating UserDetailsServiceImpl

--- src/main/java/com/envn8/app/service/UserDetailsServiceImpl.java
+++ src/main/java/com/envn8/app/service/UserDetailsServiceImpl.java
@@ -1,5 +1,25 @@
 package com.envn8.app.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public class UserDetailsServiceImpl {
+import com.envn8.app.models.User;
+import com.envn8.app.repository.UserRepository;
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
 
 }
