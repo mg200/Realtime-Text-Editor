39b6316c9f6d5ac5a77f269c40306e7be82336f6
Mohamed Gamal <mg68163@gmail.com>
2024-04-26 07:56:22 +0300

    editing an error

--- src/main/java/com/envn8/app/controller/DocumentController.java
+++ src/main/java/com/envn8/app/controller/DocumentController.java
@@ -133,6 +133,37 @@ public class DocumentController {
 
 
 
+    static class DocumentRequest {
+        private String content;
+        private String title;
+        private String type;
+        // Getters and setters
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+    }
 
     static class ShareRequest {
         private String username; // This represents the user that the document is shared with
