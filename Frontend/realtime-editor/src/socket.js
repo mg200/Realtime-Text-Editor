44143bb68df6acd84b91956b53a5e4b4e17ba8fa
Ahmed <81365347+ahmedwaleed01@users.noreply.github.com>
2024-05-15 03:01:22 +0300

    edit logo name and remove unwanted files

--- Frontend/realtime-editor/src/socket.js
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://hmamdocs.me/api/topic";
-
-export const socket = io(URL);
